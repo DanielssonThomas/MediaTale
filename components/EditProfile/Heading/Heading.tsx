9578e9ff1fdc2d09958e37dfd90e9e2ac3e56d2e
Thomas Danielsson <69715206+DanielssonThomas@users.noreply.github.com>
2023-10-23 16:22:57 +0200

    Removed: Follower and following count for edit page

--- components/EditProfile/Heading/Heading.tsx
+++ components/EditProfile/Heading/Heading.tsx
@@ -57,20 +57,6 @@ const EditProfileHeading = ({
           />
         </div>
       </div>
-      <div>
-        <div className="flex justify-between w-full h-[2rem]">
-          <h3 className="w-full border-solid border-black dark:border-white border-r-[1px] px-2 text-sm">
-            Followers:
-          </h3>
-          <h3 className="w-full px-2 ">Following:</h3>
-        </div>
-        <div className="flex justify-between w-full h-[2rem] border-solid border-black dark:border-white border-b-[1px]">
-          <h4 className="w-full px-2 border-solid border-black dark:border-white border-r-[1px]">
-            {followers}
-          </h4>
-          <h4 className="w-full px-2">{following}</h4>
-        </div>
-      </div>
     </div>
   );
 };
