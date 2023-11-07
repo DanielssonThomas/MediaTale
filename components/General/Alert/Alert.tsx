36ae1e61177203db505e03ae6360a27875d220d1
Thomas Danielsson <69715206+DanielssonThomas@users.noreply.github.com>
2023-11-07 15:18:06 +0100

    add: created alert message
    Features and refinement is needed. However now you can use <Alert error={boolean} text={contents} /> to have a message pop up for a few seconds, or removed by click

---
+++ components/General/Alert/Alert.tsx
@@ -0,0 +1,33 @@
+"use client";
+import { useEffect, useState } from "react";
+
+type AlertProps = {
+  error: boolean;
+  text: string;
+};
+
+export const Alert = ({ error, text }: AlertProps) => {
+  const [show, setShow] = useState<boolean>(true);
+  useEffect(() => {
+    setTimeout(() => setShow(!show), 3000);
+  }, []);
+  return (
+    <div className="absolute w-screen h-[8rem] flex justify-center items-center top-0">
+      <div
+        className={`absolute flex flex-col justify-center items-center border-[1px] border-solid border-black dark:border-white w-[20rem] h-[3rem] rounded-lg ${
+          show === true ? "top-[1rem]" : "top-[-4rem]"
+        } ${
+          error ? "bg-red-500 " : "bg-green-500"
+        } text-white transition-all z-50`}
+      >
+        <p
+          className="absolute flex justify-center items-center top-[-0.5rem] right-[-0.5rem] bg-black rounded-full p-1 cursor-pointer w-8 h-8 text-xl"
+          onClick={() => setShow(!show)}
+        >
+          X
+        </p>
+        <h2>{text}</h2>
+      </div>
+    </div>
+  );
+};
