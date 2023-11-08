0530d17a05dd36357692f14f95eeefd02081649a
Thomas Danielsson <69715206+DanielssonThomas@users.noreply.github.com>
2023-11-08 12:28:36 +0100

    add: toast message for comments

--- app/(pages)/post/[postId]/page.tsx
+++ app/(pages)/post/[postId]/page.tsx
@@ -1,6 +1,7 @@
 import Navigation from "@/components/Navigation";
 import Post from "@/components/Post/Post";
 import { createServerActionClient } from "@supabase/auth-helpers-nextjs";
+import Toast from "@/components/General/Toast";
 import { cookies } from "next/headers";
 import {
   getPostById,
@@ -11,19 +12,27 @@ import {
 
 type PostProps = {
   params: { postId: number };
+  searchParams: { message: string; error: boolean };
 };
 
-const PostPage = async ({ params: { postId } }: PostProps) => {
+const PostPage = async ({
+  params: { postId },
+  searchParams: { message, error },
+}: PostProps) => {
   const supabase = createServerActionClient({ cookies });
   const post = await getPostById({ post_id: postId });
 
   const profile = await getProfileById({ user_id: post?.created_by_uuid });
   const postStatistics = await getPostStatisticsById({ post_id: postId });
   const comments = await getCommentsByPostId({ post_id: postId });
+  const showToast = message ? true : false;
+
   const theme = cookies().get("theme");
   return (
     <div className={theme?.value}>
       <div className="bg-white dark:bg-black min-h-[100vh]">
+        {showToast === error && <Toast error={true} text={message} />}
+        {showToast && message !== "" && <Toast error={false} text={message} />}
         <Navigation isLoggedIn={true} />
         <Post
           authorStatistics={profile}
