615d9a19a0823185bac419c53ef264c164c33984
Thomas Danielsson <69715206+DanielssonThomas@users.noreply.github.com>
2023-11-13 19:15:09 +0100

    fix: loading donut color

--- app/loading.tsx
+++ app/loading.tsx
@@ -4,7 +4,7 @@ const Loading = async () => {
   return (
     <div className="flex flex-col justify-center items-center w-screen h-screen bg-[#EDEDED] dark:bg-[#1C1C1C]">
       <div
-        className="inline-block h-8 w-8 animate-spin rounded-full border-4 border-solid border-current border-r-transparent align-[-0.125em] motion-reduce:animate-[spin_1.5s_linear_infinite]"
+        className="inline-block h-8 w-8 animate-spin rounded-full border-4 border-solid border-current border-r-transparent align-[-0.125em] motion-reduce:animate-[spin_1.5s_linear_infinite] text-black dark:text-white"
         role="status"
       >
         <span className="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]">
